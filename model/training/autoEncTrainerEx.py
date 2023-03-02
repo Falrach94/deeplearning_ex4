384a7e82721b230587b913aefea7f5b5242924ab
Falrach94 <BenjaminZehrer@gmx.de>
2023-03-02 21:56:28 +0100

    minor

--- model/training/autoEncTrainerEx.py
+++ model/training/autoEncTrainerEx.py
@@ -11,15 +11,15 @@ class AutoEncTrainerEx(AutoEncTrainer):
     def __init__(self, cf, aef, ld):
         super().__init__()
 
-        self.image_loss = torch.nn.MSELoss()
-        self.classifier_loss = AsymmetricLossOptimized(3)
-        self.sparse_loss = torch.nn.L1Loss()
-        #self.classifier_loss = torch.nn.BCELoss()
+        self.image_loss = torch.nn.MSELoss().cuda()
+        self.classifier_loss = AsymmetricLossOptimized(3).cuda()
 
         self.a = aef
         self.b = cf
         self.ld = ld
 
     def calc_loss(self, input, pred, label):
-        return self.a * self.image_loss(pred[0], input) + self.b * self.classifier_loss(pred[1], label) + self.ld*np.sum(np.abs(pred[2]))
+        return self.a * self.image_loss(pred[0], input) \
+            + self.b * self.classifier_loss(pred[1], label) \
+            + self.ld*torch.sum(torch.abs(pred[2]))
 

aa25498f7a3f373afdd7a9bfb2353db899328a00
Falrach94 <BenjaminZehrer@gmx.de>
2023-03-03 03:10:09 +0100

    minor

--- model/NNModels/ResNet50v2_pre.py
+++ model/NNModels/ResNet50v2_pre.py
@@ -11,24 +11,24 @@ class ResNet50v2_Pretrained(torch.nn.Module):
         self.model = tv.models.wide_resnet50_2(
             weights=tv.models.Wide_ResNet50_2_Weights.DEFAULT)
 
-        for param in self.model.parameters():
-            param.requires_grad = False
+     #   for param in self.model.parameters():
+     #       param.requires_grad = False
         for param in self.model.layer4.parameters():
             param.requires_grad = True
 
 
 
-        self.classifier = torch.nn.Sequential(
-            torch.nn.Dropout(p=0.5),
-            torch.nn.ReLU(inplace=True),
-            torch.nn.Linear(512, 2),
-            torch.nn.Sigmoid()
-        )
-        self.model.fc = torch.nn.Linear(2048, 512)
+        #self.classifier = torch.nn.Sequential(
+        #    torch.nn.Dropout(p=0.5),
+        #    torch.nn.ReLU(inplace=True),
+        #    torch.nn.Linear(512, 2),
+        #    torch.nn.Sigmoid()
+        #)
+        self.model.fc = torch.nn.Linear(2048, 2)
 
- #       self.sigmoid = torch.nn.Sigmoid()
+        self.sigmoid = torch.nn.Sigmoid()
 
     def forward(self, x):
         x = self.model(x)
-        x = self.classifier(x)
+        x = self.sigmoid(x)
         return x

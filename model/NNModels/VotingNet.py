541a9e6dbee9551cacdeebfba4118ecfa99bf58d
Falrach94 <BenjaminZehrer@gmx.de>
2023-03-05 22:35:32 +0100

    d

--- model/NNModels/VotingNet.py
+++ model/NNModels/VotingNet.py
@@ -8,13 +8,13 @@ from model.NNModels.ResNet import ResNet
 from model.NNModels.ResNet34_pre import ResNet34_Pretrained
 from model.preprocessing.freq_filter import FrequencyFilter
 
-
+VOTER_CNT = 5
 class VotingNet(torch.nn.Module):
 
     def __init__(self):
         super().__init__()
 
-        self.voters = [ResNet34_Pretrained() for _ in range(5)]
+        self.voters = [ResNet34_Pretrained() for _ in range(VOTER_CNT)]
 
         self.voter1 = self.voters[0]
         self.voter2 = self.voters[1]
@@ -28,7 +28,7 @@ class VotingNet(torch.nn.Module):
     def create():
         net = VotingNet()
 
-        state = [torch.load(f'assets/best_model{i}.ckp') for i in range(5)]
+        state = [torch.load(f'assets/best_model{i}.ckp') for i in range(VOTER_CNT)]
         for n, s in zip(net.voters, state):
             n.load_state_dict(s)
 
@@ -36,14 +36,14 @@ class VotingNet(torch.nn.Module):
         return net
 
     def forward(self, x):
-
+        print('x', x.shape)
         y = [net(x)[:, None, :] for net in self.voters]
         x = torch.cat(y, dim=1)
         print('after cat', x.shape)
         conf = torch.maximum(x, 1-x)
         max_ix = torch.argmax(conf, dim=1)
         print('max_ix', max_ix.shape)
-        mask = torch.nn.functional.one_hot(max_ix)
+        mask = torch.nn.functional.one_hot(max_ix, VOTER_CNT)
         mask = torch.transpose(mask, 1, 2)
         print('mask', mask.shape)
         mask = mask > 0.5

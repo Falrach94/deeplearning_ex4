16cfb2c573f4915ffbfb4bf8d1c05b85eb17b570
Falrach94 <BenjaminZehrer@gmx.de>
2023-03-06 15:07:34 +0100

    sxy

--- main scripts/cli_mp.py
+++ main scripts/cli_mp.py
@@ -36,13 +36,13 @@ METRIC_CALC = calc_multi_f1
 SELECT_BEST_METRIC = select_best_metric
 
 # optimizer
-lr = 0.0001
-decay = 0.0001
+lr = 0.00003
+decay = 0.00001
 OPTIMIZER_FACTORY = AdamFactory(decay, lr)
 
 # loss fct
 gamma_neg = 3
-gamma_pos = 2
+gamma_pos = 3
 clip = 0.05
 
 loss_calculator = ASLCalculator(gamma_neg, gamma_pos, clip)

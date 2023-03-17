fb970a6a8534355b2b1a781d3ab7591a1af4b064
Falrach94 <BenjaminZehrer@gmx.de>
2023-03-18 00:31:57 +0100

    t

--- model/NNModels/ResNet50v2_pre.py
+++ model/NNModels/ResNet50v2_pre.py
@@ -14,18 +14,15 @@ class ResNet50v2_Pretrained(torch.nn.Module):
         self.model = tv.models.wide_resnet50_2(
             weights=tv.models.Wide_ResNet50_2_Weights.DEFAULT)
         self.model.fc = nn.Sequential(
-            nn.Linear(2048, 512),
-            nn.Dropout(p=0.5),
-            nn.ReLU(inplace=True),
-            nn.Linear(512, 2)
+            nn.Linear(2048, 4),
+            nn.Softmax(1)
         )
         for module in self.model.fc.modules():
             if isinstance(module, nn.Linear):
                 init.xavier_uniform_(module.weight)
 
-        self.sigmoid = torch.nn.Sigmoid()
+#        self.sigmoid = torch.nn.Sigmoid()
 
     def forward(self, x):
         x = self.model(x)
-        x = self.sigmoid(x)
         return x

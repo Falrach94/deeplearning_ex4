f0b37cd37b1e2d1163dff651e2f16905a495af90
Falrach94 <BenjaminZehrer@gmx.de>
2023-03-21 14:40:48 +0100

    t

--- cli_program/settings/model_settings.py
+++ cli_program/settings/model_settings.py
@@ -5,7 +5,7 @@ from model.NNModels.ResNet34_pre import ResNet34_Pretrained
 from model.NNModels.ResNet50v2_pre import ResNet50v2_Pretrained
 
 #MODEL = ResNet34_Pretrained()
-MODEL = ResNetAutoEncoder(256)
+MODEL = ResNetAutoEncoder()
 #MODEL = InceptionV3()
 #MODEL = ResNet50v2_Pretrained()
 

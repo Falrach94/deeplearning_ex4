67240bdc46c82082c5444db42e19f2602e1f023d
Falrach94 <BenjaminZehrer@gmx.de>
2023-03-27 19:49:36 +0200

    t

--- utils/utils.py
+++ utils/utils.py
@@ -21,7 +21,7 @@ def export(model: nn.Module, state, path, sb):
                       x,  # model input (or a tuple for multiple inputs)
                       path + '.zip',  # where to save the model (can be a file or file-like object)
                       export_params=True,  # store the trained parameter weights inside the model file
-                      opset_version=10,  # the ONNX version to export the model to
+                      opset_version=11,  # the ONNX version to export the model to
                       do_constant_folding=True,  # whether to execute constant folding for optimization
                       input_names=['input'],  # the model's input names
                       output_names=['output'],  # the model's output names

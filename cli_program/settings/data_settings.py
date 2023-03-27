81a5b1f9d8f346777175ccd5ee988d7afa75de61
Falrach94 <BenjaminZehrer@gmx.de>
2023-03-27 18:18:43 +0200

    t

--- cli_program/settings/data_settings.py
+++ cli_program/settings/data_settings.py
@@ -16,7 +16,7 @@ CSV_SEPERATOR = ','
 #LABEL_COLUMNS = ['inactive']
 LABEL_COLUMNS = ['crack', 'inactive']
 
-HOLDOUT_SPLIT = 0.15
+HOLDOUT_SPLIT = 0.1
 
 AUGMENTATIONS = [
     lambda x: mirror_and_rotate(x, False, False, 1),
@@ -82,10 +82,10 @@ VAL_TRANSFORMS = tv.transforms.Compose([tv.transforms.ToPILImage(),
 #LABEL_PROVIDER = SimpleLabeler(*LABEL_COLUMNS, output_mode='auto')
 #FUSER = SimpleFuser()
 
-LABEL_PROVIDER = SimpleLabeler(*LABEL_COLUMNS, output_mode='raw')
+#LABEL_PROVIDER = SimpleLabeler(*LABEL_COLUMNS, output_mode='raw')
 #FUSER = BalancedFuser(LABEL_PROVIDER, None, oversample=False)
 
-#LABEL_PROVIDER = SingleLabeler(*LABEL_COLUMNS, output_mode='raw')
+LABEL_PROVIDER = SingleLabeler(*LABEL_COLUMNS, output_mode='raw')
 FUSER = BalancedFuser(LABEL_PROVIDER, None, oversample=True)
 
 
@@ -94,7 +94,7 @@ FUSER = BalancedFuser(LABEL_PROVIDER, None, oversample=True)
 TR_FILTER = None# SmallSetFilter(0.05)
 VAL_FILTER = None
 
-TR_FILTER = OnlyDefectsFilter()# SmallSetFilter(0.05)
-VAL_FILTER = OnlyDefectsFilter()
+#TR_FILTER = OnlyDefectsFilter()# SmallSetFilter(0.05)
+#VAL_FILTER = OnlyDefectsFilter()
 
 AUGMENTER = CustomAugmentor(FUSER, AUGMENTATIONS)

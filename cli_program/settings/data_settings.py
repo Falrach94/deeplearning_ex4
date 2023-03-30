f594a971efc81b7f4085cb8fdf5f25b36efd7177
Falrach94 <BenjaminZehrer@gmx.de>
2023-03-30 15:13:17 +0200

    t

--- cli_program/settings/data_settings.py
+++ cli_program/settings/data_settings.py
@@ -71,7 +71,7 @@ AUGMENTATIONS = [
 AUGMENTATIONS = [
     lambda x: mirror_and_rotate(x, True, False, 0),
     lambda x: mirror_and_rotate(x, False, True, 0),
-    lambda x: mirror_and_rotate(x, True, True, 0),  
+    lambda x: mirror_and_rotate(x, True, True, 0),
     lambda x: mirror_and_rotate(x, False, False, 1),
     lambda x: mirror_and_rotate(x, False, False, 2),
     lambda x: mirror_and_rotate(x, False, False, 3),
@@ -83,5 +83,5 @@ AUGMENTER_CONFIG = {
 
 VAL_FILTER_TYPE = FilterTypes.NO_AUGS
 VAL_FILTER_CONFIG = None
-TR_FILTER_TYPE = #FilterTypes.NO_AUGS #FilterTypes.SMALL_SET
+TR_FILTER_TYPE = None#FilterTypes.NO_AUGS #FilterTypes.SMALL_SET
 TR_FILTER_CONFIG = None #{'size': 0.05}
